111e75272db95df92294f07a7e78d696ed42eb48
Erik Brudzinskis <brudzinskise@gmail.com>
2024-02-11 12:25:56 +0200

    Change App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Change by feature branch!
+          Change by feature branch 1!
         </p>
         <a
           className="App-link"
